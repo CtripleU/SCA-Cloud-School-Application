2e93041b09eeed3d9f2d1104727f4642f676bf53
ctripleu <cuoyemike@gmail.com>
2021-07-20 02:47:20 +0100

    second commit

---
+++ new.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+import pytz
+
+  
+WAT = pytz.timezone('Africa/Lagos')
+now = datetime.now(WAT)
+
+current_time = now.strftime("%H:%M:%S")
+print("The time in Nigeria is", current_time)
