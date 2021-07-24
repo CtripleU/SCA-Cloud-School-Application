1ed72570dc70dd18f9f2b9bc288215602d2bb397
Cumi Oyemike <cuoyemike@gmail.com>
2021-07-24 07:00:18 +0100

    Small changes for GitHub Jenkins Webhook final test

--- new.py
+++ new.py
@@ -2,5 +2,5 @@ from datetime import datetime
 
 now = datetime.now() 
 
-current_time = now.strftime("%H+:%M:%S")
+current_time = now.strftime("%H:%M:%S")
 print("The GMT time right now is", current_time)
