37d10bd9283fdd323745967cca04f1b829068805
Cumi Oyemike <cuoyemike@gmail.com>
2021-07-24 06:40:41 +0100

    Testing the GithHub Jenkins WebHook

--- new.py
+++ new.py
@@ -3,4 +3,4 @@ from datetime import datetime
 now = datetime.now()
 
 current_time = now.strftime("%H:%M:%S")
-print("The time right now in Nigeria is", current_time)
+print("The time now in Nigeria is", current_time)
